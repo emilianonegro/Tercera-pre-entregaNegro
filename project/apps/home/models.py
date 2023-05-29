4502c94ace345042941b5bf5a579487148e7bce6
Emiliano Negro <emilianonegro93@gmail.com>
2023-05-29 17:29:07 -0300

    Created models in home

--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+ title = models.CharField(max_length=100)
+ content = models.TextField()
+ def __str__(self) -> str:
+  return self.title
+ 
+class Author(models.Model):
+ name = models.CharField(max_length=100)
+ age = models.IntegerField()
+ 
+ def __str__(self) -> str:
+  return self.name
