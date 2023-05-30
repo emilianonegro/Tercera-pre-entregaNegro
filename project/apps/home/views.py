f74b2b0cdab65235b6641285ec538df7b867b4a1
Emiliano Negro <emilianonegro93@gmail.com>
2023-05-29 21:39:43 -0300

    search in db

--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from . import forms
+from . import forms, models
 
 def index(request):
     return render(request, 'home/index.html')
@@ -23,4 +23,14 @@ def create_recipe(request):
             return redirect("home:index")
     else:
         form = forms.RecipeForm()
-    return render(request, "home/create_recipe.html", {"form": form})
+
+    # Obtener el parámetro de búsqueda desde la solicitud POST o GET
+    search_query = request.GET.get('search_query')
+
+    # Realizar la consulta a la base de datos si hay un parámetro de búsqueda
+    if search_query:
+        recipes = models.Recipe.objects.filter(title__icontains=search_query)
+    else:
+        recipes = models.Recipe.objects.all()
+
+    return render(request, "home/create_recipe.html", {"form": form, "recipes": recipes})
